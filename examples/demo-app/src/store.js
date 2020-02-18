e4ffd98daaf50baf1c1e1bbaf6f08ecec9b7be41
Giuseppe Macri <gmacri@uber.com>
2020-02-18 14:53:07 -0500

    Added logic to handle mulitple datasets per filter
    Signed-off-by: Giuseppe Macri <gmacri@uber.com>

--- examples/demo-app/src/store.js
+++ examples/demo-app/src/store.js
@@ -45,7 +45,6 @@ let composeEnhancers = compose;
 /**
  * comment out code below to enable Redux Devtools
  */
-/*
 if (window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) {
   composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
     actionsBlacklist: [
@@ -54,7 +53,6 @@ if (window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) {
       '@@kepler.gl/LAYER_HOVER'
     ]
   })
-};
-*/
+}
 
 export default createStore(reducers, initialState, composeEnhancers(...enhancers));
